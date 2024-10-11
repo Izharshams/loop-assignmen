c0db9f38775bf95098e42c1d3a7821d13a0ad8aa
izharshams <izharshams173@gmail.com>
2024-10-10 23:55:28 -0700

    loop prectice

---
+++ app.js
@@ -0,0 +1,24 @@
+// for(i = 1; i <= 6; i++){
+//     document.write("<br>")
+//     for(k = 1; k <= 6-i; k++){
+//         document.write("&nbsp;&nbsp")
+//     }
+//     for(j + 1; j<=i; j++){
+//         document.write("*")
+//     }
+//     for(l = 2; l<=i; l++){
+//         document.write("*")
+//     }
+//    }
+for (i = 1; i <= 6; i++){
+    document.write("</br>")
+    for(k = 1; k <=6-i; k++){
+     document.write("&nbsp&nbsp")
+    }
+    for(j = 1; j <=i; j++){
+       document.write("*")
+    }
+    for(l = 2; l <=i; l++){
+       document.write("*")
+    }
+ }
